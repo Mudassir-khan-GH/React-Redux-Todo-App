0b77dda64c67a7f827dad6f6beba1aa8fe0ce876
Mudassir <mudassirpak47#gmail.com>
2025-11-14 15:58:46 +0500

    Add axios for API calls and implement data fetching in Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Input from './Input'
 import List from './List'
+import axios from 'axios'
 
 function Main() {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get("http://localhost:8000/api/v1/user/read")
+            setData(response.data)
+        }
+        fetchData()
+    }, [])
+
+    console.log(data);
+    
+
     return (
         <div className='min-h-screen w-full flex flex-col justify-center items-center bg-[#edeae1] gap-10 py-7 '>
             <Input />
